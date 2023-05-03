0dfe21ae3e770a2d9e1cf87da74d79159a5d3a8c
Ankur Parchani <ankurparchani@gmail.com>
2023-05-03 14:34:47 +0530

    videos changes

--- home.js
+++ home.js
@@ -2,7 +2,6 @@ const header = document.querySelector(".header");
 const video = document.createElement("video");
 const videoParent = document.createElement("div");
 const onScreenLogo = document.querySelector(".onScreenLogo");
-const dropDownArrow = document.querySelector(".dropdown_arrow");
 const introVideo = document.querySelector(".introVideo");
 
 // Appending the starter video
@@ -11,7 +10,6 @@ introVideo.addEventListener("ended", function () {
 });
 
 if (window.innerWidth < 600) {
-  dropDownArrow.classList.add("hidden");
   header.classList.remove("hidden");
   dropDownArrow.style.display = "none";
   onScreenLogo.style.display = "none";
